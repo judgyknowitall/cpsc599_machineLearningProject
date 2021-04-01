dab95333f7e9271cdac4ffafc5903d902b140943
Abdullah Al-Wadeiah <abdullah.aw14@gmail.com>
2021-04-01 16:22:01 -0600

    completed unopitmized logistic regression, found bug in processed data

--- src/logReg.py
+++ src/logReg.py
@@ -8,8 +8,54 @@ Team 7
 Logistic Regression model
 """
 
+import pandas as pd
+from numpy import mean
+from numpy import std
+from collections import Counter
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import RepeatedStratifiedKFold
+
 def train_logReg(data):
     
     print("Training Logistic Regression Model ...")
+    # For multiclass problems, can only use ‘newton-cg’, ‘sag’, ‘saga’ and ‘lbfgs’
+    model = LogisticRegression(multi_class='multinomial', solver='lbfgs')
     
-    # TODO...
+    # Separate labels from data
+    labels = data["Severity"]
+    data.drop("Severity", axis=1, inplace=True)
+    data.drop("Contact", axis=1, inplace=True)
+    
+    X_train, X_test, y_train, y_test = train_test_split(
+        data, labels, stratify=labels, test_size=0.3)
+
+    # Training set summary
+    # print(X_train.shape, y_train.shape)
+    # print(Counter(y_train))
+    
+    # Define the model evaluation procedure
+    cv = RepeatedStratifiedKFold(n_splits=10, n_repeats=3, random_state=1)
+
+    # Evaluate the model and collect the scores
+    n_scores = cross_val_score(
+        model, X_train, y_train, scoring='accuracy', cv=cv, n_jobs=-1)
+    
+    print('Mean Accuracy: %.3f (%.3f)' % (mean(n_scores), std(n_scores)))
+
+    
+    model.fit(X_train, y_train)
+    
+    print("Model accuracy: {:.3f}".format(model.score(X_test, y_test)))
+
+
+'''
+Driver function
+'''
+def main():
+    data = pd.read_csv("../data/Processed-Data.csv")
+    train_logReg(data)
+
+if __name__ == "__main__":
+    main()
