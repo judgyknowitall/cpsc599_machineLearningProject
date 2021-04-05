7b86d6d4c5d1f79fb2e69c5cfd93718cb5c78381
xper28 <ahmad.hassan1@ucalgary.ca>
2021-04-05 17:23:16 -0600

    Ignore previous commit

--- src/dnn.py
+++ src/dnn.py
@@ -19,12 +19,12 @@ import pandas
 
 def read_data():
     #Read the data in and perform the split
-    filename = '../data/Processed-Data.csv'
+    filename = '../data/Experimental-Data.csv'
     data = pandas.read_csv(filename)
     print(data.shape[0])
 
     
-    X_train, X_test, y_train, y_test = train_test_split(data, data.Severity, train_size=0.1, test_size=0.9, random_state=0)
+    X_train, X_test, y_train, y_test = train_test_split(data.drop('Severity', axis=1), data.Severity, train_size=0.9, test_size=0.1)
     return X_train, X_test, y_train, y_test
 
 def train_dnn():
@@ -39,7 +39,7 @@ def train_dnn():
     #Create model
     model = Sequential()
     model.add(Dense(len(list(X_train)), activation='relu'))
-    model.add(Dense(4, activation='relu'))
+    model.add(Dense(8, activation='relu'))
     model.add(Dense(4, activation='softmax'))
     
     model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
